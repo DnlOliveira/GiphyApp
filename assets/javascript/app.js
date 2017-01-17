21c88a5e34f41dad93fe520b4927b78f7c56b22c
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-01-16 23:24:59 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,13 +30,41 @@ var app = {
 		$("#search").append(submit);
 
 	},
-	search: function(){
-		var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+	search: function(value){
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        value + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     	$.ajax({
       		url: queryURL,
       		method: 'GET'
     	}).done(function(response) {
+    		var results = response.data;
+
+    		for (var i = 0; i < results.length; i++) {
+    			var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              	var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              	var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              	var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              	personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              	gifDiv.append(p);
+              	gifDiv.append(personImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              	$("#display").prepend(gifDiv);
+    		}
+
+
       		console.log(response);
     	});
 	},
@@ -47,6 +75,7 @@ app.start();
 
 $(document).on("click", "#topic", function(){
 	var value = $(this).val();
+	app.search(value);
 	console.log(value);
 });
 
